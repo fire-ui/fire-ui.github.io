5d38adfa19fa370ada43efdd9a335d86baa24e25
kimlimjustin <kimlimjustin@gmail.com>
2021-01-16 13:56:20 +0700

    Added marquee docs

--- src/utils/FireUIJS.js
+++ src/utils/FireUIJS.js
@@ -114,4 +114,14 @@ export const doStuff = () => {
             })
         }
     })
+    var marquee = document.querySelectorAll('marquee');
+    for(let i=0; i<marquee.length; i++){
+        marquee[i].setAttribute('scrollamount', '10');
+    }
+    for(let x=0; x<marquee.length; x++){
+        if(marquee[x].hasAttribute('data-speed')){
+            var speed = document.getElementsByTagName('marquee')[x].getAttribute('data-speed');
+            document.getElementsByTagName('marquee')[x].setAttribute('scrollamount', speed);
+        }
+    }
 }
